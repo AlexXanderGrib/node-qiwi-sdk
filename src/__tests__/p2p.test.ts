e547187239b4944f955eac772af8c45fa68787a4
AlexXanderGrib <pockomhadzzop@gmail.com>
2020-11-19 11:56:09 +0300

    docs(): Added emojis and table of contents to README.md

--- src/__tests__/p2p.test.ts
+++ src/__tests__/p2p.test.ts
@@ -9,33 +9,54 @@ config();
 
 describe("P2P", () => {
   const qiwi = new P2P(process.env.QIWI_PK as string);
+
   const billId = uuid();
+  const amount = {
+    currency: Math.random() >= 0.5 ? P2P.Currency.RUB : P2P.Currency.KZT,
+    value: 20 + Math.round(Math.random() * 100)
+  };
 
   test("Can be initialized", () => expect(qiwi).toBeTruthy());
 
   test("Can create Bill", async () => {
     const date = P2P.formatLifetime(1 + Math.round(Math.random() * 39));
 
-    const response = await qiwi
-      .createBill(
-        {
-          amount: {
-            currency: P2P.Currency.RUB,
-            value: 10
-          },
+    const response = await qiwi.createBill(
+      {
+        amount,
+        expirationDateTime: date,
+        comment: "Тест",
+        customer: {},
+        customFields: {}
+      },
+      billId
+    );
 
-          expirationDateTime: date,
-          comment: "Тест",
-          customer: {},
-          customFields: {}
-        },
-        billId
-      )
-      .catch(console.error);
+    expect(typeof response?.payUrl).toBe("string");
 
-    expect(response).toHaveProperty("billId");
-    expect(response).toHaveProperty("payUrl");
-    expect(response).toHaveProperty("siteId");
+    expect(response?.status?.value).toBe(P2P.BillStatus.WAITING);
+    expect(response?.billId).toBe(billId);
+    expect(response?.amount?.currency).toStrictEqual(amount.currency);
+  });
+
+  test("Can get bill status", async () => {
+    const response = await qiwi.billStatus(billId);
+
+    expect(typeof response?.payUrl).toBe("string");
+
+    expect(response?.status?.value).toBe(P2P.BillStatus.WAITING);
+    expect(response?.billId).toBe(billId);
+    expect(response?.amount?.currency).toStrictEqual(amount.currency);
+  });
+
+  test("Can delete bill", async () => {
+    const response = await qiwi.rejectBill(billId);
+
+    expect(typeof response?.payUrl).toBe("string");
+
+    expect(response?.status?.value).toBe(P2P.BillStatus.REJECTED);
+    expect(response?.billId).toBe(billId);
+    expect(response?.amount?.currency).toStrictEqual(amount.currency);
   });
 
   test("Cant create Bill date back", async () => {
@@ -46,6 +67,8 @@ describe("P2P", () => {
         expirationDateTime: date,
         amount: { currency: P2P.Currency.RUB, value: 99 }
       });
+
+      expect(false).toBeTruthy();
     } catch (error) {
       expect(error).toBeInstanceOf(P2PPaymentError);
       expect(error.message).toContain("Validation error");
