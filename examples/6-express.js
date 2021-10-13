21e41de35055de9789b1939bf686e26fb788728c
Alexander Gribochkin <me@alexxgrib.me>
2021-10-13 09:03:45 +0300

    docs(): Added example for express

---
+++ examples/6-express.js
@@ -0,0 +1,41 @@
+// const { P2P } = require('qiwi-sdk');
+const { P2P, P2PNotificationError } = require("..");
+
+const express = require("express");
+const app = express();
+const p2p = new P2P(process.env.QIWI_SECRET_KEY, process.env.QIWI_PUBLIC_KEY);
+const port = parseInt(process.env.PORT) || 3000;
+
+app.get("/pay", async (req, res) => {
+  const bill = await p2p.createBill({
+    amount: { value: 10, currency: P2P.Currency.RUB },
+    expirationDateTime: P2P.formatLifetime(2),
+  });
+
+  const url = P2P.patchPayUrl(bill.payUrl, {
+    successUrl: `https://localhost:${port}/success`,
+    paySource: P2P.PaySource.Card
+  });
+
+  res.redirect(url);
+});
+
+app.get("/success", (req, res) => {
+  res.end("Спасибо за покупку!")
+});
+
+app.post(
+  "/webhook/qiwi",
+  p2p.notificationMiddleware({}, (req, res) => {
+    req.body // BillStatusData
+  })
+);
+
+
+app.use((error, req, res, next) => {
+  if(error instanceof P2PNotificationError) {
+    console.log(error) // Кто-то отправил невалидное уведомление
+  }
+
+  return next();
+});
